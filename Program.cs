acb5b21c35b4662cd846edca9c05c78482c3385a
Stephen2Page <stephen2page@gmail.com>
2016-09-09 17:05:08 -0400

    completed age processing and tested

--- Program.cs
+++ Program.cs
@@ -16,25 +16,31 @@ namespace FortuneTeller
             int age = 21;
             int yearsToRetirement;
             string birthMonth;
-            string favColor;
+            char favColor;
             string siblings;
 
             //Get data
             //check each for quit with each
 
             Console.WriteLine("Answer these questions to get your fortune");
-            //Ask the user for the user’s first name
+            
             Console.WriteLine("Enter your first name");
             firstName = Console.ReadLine();
-
-            //Ask the user for the user’s last name
+            if (firstName.ToLower() == "quit")
+            {
+                System.Environment.Exit(0);
+            }
+            
             Console.WriteLine("Enter your last name");
             lastName = Console.ReadLine();
-
-            //Ask the user for the user’s age
+            if (lastName.ToLower() == "quit")
+            {
+                System.Environment.Exit(0);
+            }
+            
             Console.WriteLine("Enter your age");
             ageInput = Console.ReadLine();
-            if (ageInput == "quit")
+            if (ageInput.ToLower() == "quit")
             {
                 System.Environment.Exit(0);
             }
@@ -43,22 +49,23 @@ namespace FortuneTeller
                 //parse ageinput to int age
                 age = int.Parse(ageInput);
             }
-            //Ask the user for the user’s birth month (as an 'int')
+
             Console.WriteLine("Enter your birth month (1-12)");
             birthMonth = Console.ReadLine();
 
-            //Ask the user for the user’s favorite ROYGBIV color
-            Console.WriteLine("Enter your favorite ROYGBIV color, If you do not know what ROYGBIV is enter HELP");
+            
+            Console.WriteLine("Enter your favorite ROYGBIV color, If you do not know what ROYGBIV is then enter HELP");
             favColor = Console.ReadLine();
 
             //If the user does not know what ROYGBIV is, ask them to enter “Help” to get a list of the ROYGBIV colors
-            //check if help and then provide list, otherwise store color and move on.
+            //check if help and then provide list,  red, orange, yellow, green, blue, indigo and violet, otherwise store color and move on.
+            //make case neutrel
 
-            //Ask the user for the number of siblings the user has
+
             Console.WriteLine("Enter how many siblings you have");
             siblings = Console.ReadLine();
 
-            //Provide fortune
+            //PROVIDE FORTUNE
             //If the user’s age is an odd number, then they will retire in ____ years, or ___ years if their age is an even number.
             //modulas opeator
             //if age/2 has a remainder then it is an odd number
@@ -72,8 +79,11 @@ namespace FortuneTeller
             }
             //Vaction Home from siblings (five options)
             //if else
+
             //Transportation from color (seven options)
+            //assign color to int
             //switch statement
+
             //amount of money from birth month (four options including booby prize)
             //if else 
 
